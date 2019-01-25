5adf0913a92dc9b2162f41a8fc601bd9ea2ff4b5
Tobias Schlitt <toby@php.net>
2019-01-25 14:21:11 +0100

    Load default schema through class in libraries/common.

---
+++ src/php/SpecificationBundle/Domain/DefaultSchemaLoader.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Frontastic\Common\SpecificationBundle\Domain;
+
+use http\Exception\InvalidArgumentException;
+
+class DefaultSchemaLoader
+{
+    const DEFAULT_SCHEMA_DIR = __DIR__ . '/../../../js/configuration/defaultSchemas';
+
+    public function load(string $schemaType): \stdClass
+    {
+        $schemaFile = self::DEFAULT_SCHEMA_DIR . '/' . $schemaType . '.json';
+
+        if (!file_exists($schemaFile)) {
+            throw new InvalidArgumentException(sprintf(
+                'Default schema for type "%s" not found', $schemaType
+            ));
+        }
+
+        return json_decode(file_get_contents($schemaFile));
+    }
+}
