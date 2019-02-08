720f5669d810a4e332cec6773adbd9431db62bd6
Sven Koschnicke <s.koschnicke@gfxpro.com>
2019-02-08 13:52:13 +0100

    Fixed merge problem

--- src/php/SpecificationBundle/Domain/DefaultSchemaLoader.php
+++ src/php/SpecificationBundle/Domain/DefaultSchemaLoader.php
@@ -13,19 +13,12 @@ class DefaultSchemaLoader
         $schemaFile = self::DEFAULT_SCHEMA_DIR . '/' . $schemaType . '.json';
 
         if (!file_exists($schemaFile)) {
-<<<<<<< HEAD
             throw new InvalidArgumentException(
                 sprintf(
                     'Default schema for type "%s" not found',
                     $schemaType
                 )
             );
-=======
-            throw new InvalidArgumentException(sprintf(
-                'Default schema for type "%s" not found',
-                $schemaType
-            ));
->>>>>>> Fixed minor PHP code style issues
         }
 
         return json_decode(file_get_contents($schemaFile));
