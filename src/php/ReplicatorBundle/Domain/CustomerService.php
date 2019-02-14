90fea2270f4acf844d834f1bfa6cf7d8fbb4c72e
Kore Nordmann <mail@kore-nordmann.de>
2019-02-14 12:19:27 +0100

    Explode project & customer configuration by engine specification by API

--- src/php/ReplicatorBundle/Domain/CustomerService.php
+++ src/php/ReplicatorBundle/Domain/CustomerService.php
@@ -18,6 +18,30 @@ class CustomerService
 
     private $customers = null;
 
+    /**
+     * List of APIs with their default engines
+     */
+    private $apis = [
+        'product' => [
+            'engine' => 'commercetools',
+        ],
+        'account' => [
+            'engine' => 'commercetools',
+        ],
+        'cart' => [
+            'engine' => 'commercetools',
+        ],
+        'search' => [
+            'engine' => 'commercetools',
+        ],
+        'content' => [
+            'engine' => 'contentful',
+        ],
+        'media' => [
+            'engine' => 'cloudinary',
+        ],
+    ];
+
     public function __construct(string $customerDir, string $deployedCustomers)
     {
         $this->customerDir = $customerDir;
@@ -45,6 +69,19 @@ class CustomerService
         $this->customers = array_values($this->customers);
     }
 
+    private function explodeConfiguration(array $values): array
+    {
+        $baseConfiguration = array_replace_recursive($this->apis, $values);
+        foreach ($this->apis as $api => $defaultEngine) {
+            $baseConfiguration[$api] = array_replace_recursive(
+                $baseConfiguration[$api],
+                $baseConfiguration[$baseConfiguration[$api]['engine']] ?? []
+            );
+        }
+
+        return $baseConfiguration;
+    }
+
     private function parseCustomerFile(string $customerFile, $transient = false): Customer
     {
         $customer = Yaml::parse(file_get_contents($customerFile));
@@ -52,24 +89,21 @@ class CustomerService
             return $this->customers[$customer['name']];
         }
 
+        $customerConfiguration = $this->explodeConfiguration($customer['configuration'] ?? []);
+
         $this->customers[$customer['name']] = $customer = new Customer([
             'name' => $customer['name'],
             'secret' => $customer['secret'],
             'edition' => $customer['edition'] ?? 'mirco',
             'isTransient' => $transient,
-            'configuration' => array_map(
-                function (array $values) {
-                    return is_array($values) ? (object) $values : $values;
-                },
-                $customer['configuration'] ?? []
-            ),
+            'configuration' => $customerConfiguration,
             'environments' => $customer['environments'] ?? [
                 'production',
                 'staging',
                 'development',
             ],
             'projects' => array_map(
-                function (array $project) use ($customer): Project {
+                function (array $project) use ($customer, $customerConfiguration): Project {
                     return new Project([
                         'projectId' => $project['projectId'],
                         'name' => $project['name'],
@@ -77,14 +111,9 @@ class CustomerService
                         'apiKey' => $customer['secret'],
                         'previewUrl' => $project['previewUrl'] ?? null,
                         'webpackPort' => $project['webpackPort'] ?? 3000,
-                        'configuration' => array_map(
-                            function (array $values) {
-                                return is_array($values) ? (object) $values : $values;
-                            },
-                            array_replace_recursive(
-                                $customer['configuration'] ?? [],
-                                $project['configuration'] ?? []
-                            )
+                        'configuration' => array_replace_recursive(
+                            $customerConfiguration,
+                            $this->explodeConfiguration($project['configuration'] ?? [])
                         ),
                         'languages' => $project['languages'] ?? [$project['defaultLanguage'] ?? 'eng_GB'],
                         'defaultLanguage' => $project['defaultLanguage'] ?? 'eng_GB',
