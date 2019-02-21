144cd6400b8a9e15d3f65a7763c109dd32e1a42c
Kore Nordmann <mail@kore-nordmann.de>
2019-02-21 09:48:21 +0100

    Provide base class for custom mailers
    (project merge back 2019-05-06)

--- src/php/CoreBundle/Domain/Mailer.php
+++ src/php/CoreBundle/Domain/Mailer.php
@@ -4,55 +4,7 @@ namespace Frontastic\Common\CoreBundle\Domain;
 
 use Symfony\Component\Templating\EngineInterface;
 
-class Mailer
+abstract class Mailer
 {
-    private $mailer;
-    private $twig;
-    private $sender;
-
-    public function __construct(\Swift_Mailer $mailer, EngineInterface $twig, string $sender = 'support@frontastic.io')
-    {
-        $this->mailer = $mailer;
-        $this->twig = $twig;
-        $this->sender = $sender;
-
-        // Fix stupid swiftmail sendmail configuration which is not possible to
-        // configure through bundle.
-        if ($this->mailer->getTransport() instanceof \Swift_Transport_SendmailTransport) {
-            $this->mailer->getTransport()->setCommand('/usr/sbin/sendmail -t');
-        }
-    }
-
-    public function sendToUser($user, string $type, string $subject, array $parameters = array())
-    {
-        $parameters = array_merge(
-            $parameters,
-            array(
-                'user' => $user,
-                'subject' => $subject,
-            )
-        );
-
-        $message = new \Swift_Message(
-            $subject,
-            $this->renderView("Emails/$type.html.twig", $parameters),
-            'text/html'
-        );
-        $message->setFrom($this->sender);
-        $message->setTo($user->email);
-
-        try {
-            $textPart = $this->renderView("Emails/$type.txt.twig", $parameters);
-            $message->addPart($textPart, 'text/plain');
-        } catch (\InvalidArgumentException $e) {
-            // Ignore missing text part
-        }
-
-        $this->mailer->send($message);
-    }
-
-    private function renderView(string $template, array $parameters)
-    {
-        return $this->twig->render($template, $parameters);
-    }
+    abstract public function sendToUser($user, string $type, string $subject, array $parameters = array());
 }
