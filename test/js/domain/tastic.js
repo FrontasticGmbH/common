44bcfa5319a69392fd07f7767bbbee3bc926a2fc
Florian Sowade <f.sowade@suora.com>
2019-02-22 11:41:10 +0100

    Added a very basic test for Tastic

---
+++ test/js/domain/tastic.js
@@ -0,0 +1,30 @@
+import Tastic from '../../../src/js/domain/tastic'
+
+describe('Tastic', () => {
+    it('contains general settings in schema when no schema is given', () => {
+        const tastic = new Tastic()
+
+        expect(tastic.schema.schema).toHaveLength(1)
+        expect(tastic.schema.schema[0].name).toBe('General')
+    })
+
+    it('merges general settings into the schema', () => {
+        const firstSchema = {
+            name: 'First',
+            fields: [],
+        }
+        const secondSchema = {
+            name: 'Second',
+            fields: [],
+        }
+
+        const tastic = new Tastic({
+            schema: [firstSchema, secondSchema],
+        })
+
+        expect(tastic.schema.schema).toHaveLength(3)
+        expect(tastic.schema.schema[0].name).toBe('General')
+        expect(tastic.schema.schema[1]).toEqual(firstSchema)
+        expect(tastic.schema.schema[2]).toEqual(secondSchema)
+    })
+})
