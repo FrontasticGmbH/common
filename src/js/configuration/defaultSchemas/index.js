715de875de34c9ec1af582e246696544fb792206
Tobias Schlitt <toby@php.net>
2019-03-04 15:41:39 +0100

    Register default cell schema to be used in Catwalk.

--- src/js/configuration/defaultSchemas/index.js
+++ src/js/configuration/defaultSchemas/index.js
@@ -1,5 +1,7 @@
 import NodeConfigurationSchema from './nodeConfiguration'
+import CellConfigurationSchema from './cellConfiguration'
 
 export default {
     NodeConfigurationSchema: NodeConfigurationSchema,
+    CellConfigurationSchema: CellConfigurationSchema,
 }
