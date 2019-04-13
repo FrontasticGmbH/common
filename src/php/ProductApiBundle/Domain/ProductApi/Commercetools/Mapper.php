c34c629da68c525e56ba63447c196c77ed507c32
HQ\gabor.meissner <gabor.meissner@arithnea.de>
2019-04-13 07:30:00 +0200

    adapt options
    (project merge back 2019-05-06)

--- src/php/ProductApiBundle/Domain/ProductApi/Commercetools/Mapper.php
+++ src/php/ProductApiBundle/Domain/ProductApi/Commercetools/Mapper.php
@@ -63,6 +63,7 @@ class Mapper
      */
     public function dataToVariant(array $variantData, Query $query, Locale $locale): Variant
     {
+        debug($locale);
         list($price, $currency, $discountedPrice) = $this->dataToPrice($variantData, $locale);
 
         $attributes = $this->dataToAttributes($variantData, $locale);
