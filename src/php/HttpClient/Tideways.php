55a73eaf6e08875f15d407bafadcbbce939c5cbb
Kore Nordmann <mail@kore-nordmann.de>
2019-05-02 10:06:11 +0200

    Ensure our HTTP calls are recorded with Tideways
    Tideways currently has a bug not recognizing fopen-http:// calls as
    HTTP, which will be fixed on their side. Until then we use a decorator
    to implement custom instrumentation of these calls if tideways is
    active.
    
    (project merge back 2019-05-06)

---
+++ src/php/HttpClient/Tideways.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Frontastic\Common\HttpClient;
+
+use Frontastic\Common\HttpClient;
+
+class Tideways extends HttpClient
+{
+    /**
+     * @var HttpClient
+     */
+    private $aggregate;
+
+    public function __construct(HttpClient $aggregate)
+    {
+        $this->aggregate = $aggregate;
+    }
+
+    public function addDefaultHeaders(array $headers)
+    {
+        return $this->aggregate->addDefaultHeaders($headers);
+    }
+
+    public function request(
+        string $method,
+        string $url,
+        string $body = '',
+        array $headers = array(),
+        Options $options = null
+    ): Response {
+        $span = null;
+        if (class_exists(\Tideways\Profiler::class)) {
+            $span = \Tideways\Profiler::createSpan('http');
+        }
+
+        try {
+            $response = $this->aggregate->request($method, $url, $body, $headers, $options);
+        } catch (\Throwable $e) {
+            throw $e;
+        } finally {
+            if ($span) {
+                $span->annotate([
+                    'http.url' => $url,
+                    'http.status' => $response->status,
+                    'http.method' => $method,
+                    'http.body' => $body,
+                    'http.headers' => implode("\n", $headers),
+                ]);
+                $span->finish();
+            }
+        }
+
+        return $response;
+    }
+}
