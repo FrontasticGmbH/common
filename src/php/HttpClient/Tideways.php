e82085c2a74f7a2a8aadc9f8b791294d256af760
Kore Nordmann <mail@kore-nordmann.de>
2019-05-03 07:46:22 +0200

    Log faield payments and HTTP requests. Also increase Paypal timeout
    (project merge back 2019-05-06)

--- src/php/HttpClient/Tideways.php
+++ src/php/HttpClient/Tideways.php
@@ -2,6 +2,7 @@
 
 namespace Frontastic\Common\HttpClient;
 
+use \Psr\Log\LoggerInterface;
 use Frontastic\Common\HttpClient;
 
 class Tideways extends HttpClient
@@ -11,9 +12,15 @@ class Tideways extends HttpClient
      */
     private $aggregate;
 
-    public function __construct(HttpClient $aggregate)
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(HttpClient $aggregate, LoggerInterface $logger)
     {
         $this->aggregate = $aggregate;
+        $this->logger = $logger;
     }
 
     public function addDefaultHeaders(array $headers)
@@ -35,16 +42,35 @@ class Tideways extends HttpClient
 
         try {
             $response = $this->aggregate->request($method, $url, $body, $headers, $options);
-        } catch (\Throwable $e) {
+        } catch (\Exception $e) {
+            $this->logger->error(sprintf(
+                '[HTTP] Exception: %s %s (%s)'.
+                $method,
+                $url,
+                $body ?: '<null>'
+            ));
+
             throw $e;
         } finally {
+            if ($response->status >= 500) {
+                $this->logger->error(
+                    sprintf(
+                        '[HTTP] Failed Request: %s %s (%s)',
+                        $method,
+                        $url,
+                        $body ?: '<null>'
+                    ),
+                    ['status' => $response->status]
+                );
+            }
+
             if ($span) {
                 $span->annotate([
                     'http.url' => $url,
                     'http.status' => $response->status,
                     'http.method' => $method,
-                    // 'http.body' => $body,
-                    // 'http.headers' => implode("\n", $headers),
+                    'http.body' => $body,
+                    'http.headers' => implode("\n", $headers),
                 ]);
                 $span->finish();
             }
