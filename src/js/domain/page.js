a12f9728e0e4a599e91dbbf86eb430a358fdcf81
Sanja <codesanja@gmail.com>
2019-08-09 11:38:47 +0100

    Refactor.

--- src/js/domain/page.js
+++ src/js/domain/page.js
@@ -212,22 +212,6 @@ class Page {
         return this.addTastic(region, cellId, tastic.tasticType, tasticIndex + 1, tastic.configuration)
     }
 
-    isActive = (page) => {
-        switch (page.state) {
-            case 'default':
-                return true
-            case 'scheduled':
-                const now = Date.now()
-                return new Date(page.scheduledFrom) <= now && new Date(page.scheduledTo) >= now
-            default:
-                return false
-        }
-    }
-
-    isScheduledForFuture = (page) => {
-        return page.state === 'scheduled' && (new Date(page.scheduledTo) > Date.now())
-    }
-
     export () {
         return {
             pageId: this.pageId,
