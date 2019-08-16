1bdbb884b6256e1c19ad259725b45032b59a13c1
Kore Nordmann <mail@kore-nordmann.de>
2019-08-16 11:39:45 +0200

    Updated tests to changed HTTPClient default setup

--- test/php/HttpClient/FactoryTest.php
+++ test/php/HttpClient/FactoryTest.php
@@ -19,9 +19,7 @@ class FactoryTest extends TestCase
     public function setUp()
     {
         $this->factory = new Factory(
-            $this->getMockBuilder(LoggerInterface::class)->getMock(),
-            $this->getMockBuilder(\Domnikl\Statsd\Client::class)
-                ->disableOriginalConstructor()->getMock()
+            $this->getMockBuilder(LoggerInterface::class)->getMock()
         );
     }
 
@@ -31,7 +29,7 @@ class FactoryTest extends TestCase
 
         $aggregates = $this->unrollAggregates($httpClient);
 
-        $this->assertCount(3, $aggregates);
+        $this->assertCount(2, $aggregates);
     }
 
     public function testCreateSigning()
