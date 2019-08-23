2504a27703b292a768b0a2de52c7dcba1ae0ea1e
Tobias Schlitt <toby@php.net>
2019-08-23 15:27:32 +0200

    Initial draft for Contentful locale mapping.
    # Conflicts:
    #	paas/libraries/common/src/php/ContentApiBundle/Domain/ContentApi/Contentful.php

---
+++ src/php/ContentApiBundle/Domain/ContentApi/Contentful/LocaleMapper.php
@@ -0,0 +1,11 @@
+<?php
+
+interface LocaleMapper
+{
+    /**
+     * @param string $frontasticLocale
+     * @return string Locale to be sent to the CMS (frontastic format)
+     */
+    public function replaceLocale(string $frontasticLocale): string;
+}
+
