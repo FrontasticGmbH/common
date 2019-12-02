f05a8b4c5e3d720b9f369ca9beb403d6957398ab
Florian Sowade <f.sowade@suora.com>
2019-12-02 17:08:03 +0100

    Move commercetools client factory to libraries common

--- src/php/ReplicatorBundle/Domain/Project.php
+++ src/php/ReplicatorBundle/Domain/Project.php
@@ -78,4 +78,21 @@ class Project extends DataObject
      * @var Endpoint[]
      */
     public $endpoints = [];
+
+    public function getConfigurationSection(string $sectionName): object
+    {
+        if (!array_key_exists($sectionName, $this->configuration)) {
+            return new \stdClass();
+        }
+
+        $config = $this->configuration[$sectionName];
+        if (is_array($config)) {
+            $config = (object)$config;
+        }
+
+        if (!is_object($config)) {
+            throw new \RuntimeException('Invalid project configuration section ' . $sectionName);
+        }
+        return $config;
+    }
 }
