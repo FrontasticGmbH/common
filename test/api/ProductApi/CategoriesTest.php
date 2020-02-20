f97d845cc6954ccc61739e3e287da0ff859f4ce3
Florian Sowade <f.sowade@suora.com>
2020-02-20 12:14:27 +0100

    Refactor tests

--- test/api/ProductApi/CategoriesTest.php
+++ test/api/ProductApi/CategoriesTest.php
@@ -4,12 +4,18 @@ namespace Frontastic\Common\ApiTests\ProductApi;
 
 use Frontastic\Common\ApiTests\FrontasticApiTestCase;
 use Frontastic\Common\ProductApiBundle\Domain\Category;
+use Frontastic\Common\ProductApiBundle\Domain\ProductApi;
 use Frontastic\Common\ProductApiBundle\Domain\ProductApi\Query\CategoryQuery;
 use Frontastic\Common\ProductApiBundle\Domain\ProductApiFactory;
 use Frontastic\Common\ReplicatorBundle\Domain\Project;
 
 class CategoriesTest extends FrontasticApiTestCase
 {
+    /**
+     * @var array<string, ProductApi>
+     */
+    private $productApis = [];
+
     /**
      * @var ProductApiFactory
      */
@@ -23,30 +29,19 @@ class CategoriesTest extends FrontasticApiTestCase
     /**
      * @dataProvider projectAndLanguage
      */
-    public function testAllCategoriesAreNotEmpty(Project $project, ?string $language): void
+    public function testAllCategoriesAreNotEmpty(Project $project, string $language): void
     {
-        $productApi = $this->productApiFactory->factor($project);
-
-        $categories = $productApi->getCategories(new CategoryQuery([
-            'locale' => $language,
-        ]));
-
+        $categories = $this->fetchCategories($project, $language);
         $this->assertNotEmpty($categories);
     }
 
     /**
      * @dataProvider projectAndLanguage
      */
-    public function testQueryCategoriesTwiceProducesSameResult(Project $project, ?string $language): void
+    public function testQueryCategoriesTwiceProducesSameResult(Project $project, string $language): void
     {
-        $productApi = $this->productApiFactory->factor($project);
-
-        $firstCategories = $productApi->getCategories(new CategoryQuery([
-            'locale' => $language,
-        ]));
-        $secondCategories = $productApi->getCategories(new CategoryQuery([
-            'locale' => $language,
-        ]));
+        $firstCategories = $this->fetchCategories($project, $language);
+        $secondCategories = $this->fetchCategories($project, $language);
 
         $this->assertEquals($firstCategories, $secondCategories);
     }
@@ -54,59 +49,88 @@ class CategoriesTest extends FrontasticApiTestCase
     /**
      * @dataProvider projectAndLanguage
      */
-    public function testCategoriesWithHighOffsetAreEmpty(Project $project, ?string $language): void
+    public function testCategoriesWithHighOffsetAreEmpty(Project $project, string $language): void
     {
-        $productApi = $this->productApiFactory->factor($project);
-
-        $categories = $productApi->getCategories(new CategoryQuery([
-            'locale' => $language,
-            'offset' => 5000,
-        ]));
-
+        $categories = $this->fetchCategories($project, $language, null, 5000);
         $this->assertEmpty($categories);
     }
 
     /**
      * @dataProvider projectAndLanguage
      */
-    public function testFetchCategoriesWithLowLimitReturnsSameAsWithHighLimit(Project $project, ?string $language): void
+    public function testFetchCategoriesWithLowLimitReturnsSameAsWithHighLimit(Project $project, string $language): void
     {
-        $productApi = $this->productApiFactory->factor($project);
-
         $limit = 24;
         $stepSize = 2;
 
-        $categoriesFetchedInOneRequest = $productApi->getCategories(new CategoryQuery([
-            'locale' => $language,
-            'limit' => $limit,
-        ]));
+        $categoriesFetchedInOneRequest = $this->fetchCategories($project, $language, $limit);
+        $categoriesFetchedInMultipleRequests =
+            $this->fetchCategoriesInMultipleSteps($project, $language, $limit, $stepSize);
 
+        $this->assertCategoriesEqualIgnoringOrder($categoriesFetchedInOneRequest, $categoriesFetchedInMultipleRequests);
+        $this->assertGreaterThan($stepSize, count($categoriesFetchedInMultipleRequests));
+    }
+
+    /*
+     * @return Category[]
+     */
+    private function fetchCategories(Project $project, string $language, ?int $limit = null, ?int $offset = null): array
+    {
+        $productApi = $this->productApiForProject($project);
+
+        $query = new CategoryQuery(['locale' => $language]);
+
+        if ($limit !== null) {
+            $query->limit = $limit;
+        }
+        if ($offset !== null) {
+            $query->offset = $offset;
+        }
+
+        return $productApi->getCategories($query);
+    }
+
+    private function productApiForProject(Project $project): ProductApi
+    {
+        $key = sprintf('%s_%s', $project->customer, $project->projectId);
+        if (!array_key_exists($key, $this->productApis)) {
+            $this->productApis[$key] = $this->productApiFactory->factor($project);
+        }
+
+        return $this->productApis[$key];
+    }
+
+    /**
+     * @return Category[]
+     */
+    private function fetchCategoriesInMultipleSteps(
+        Project $project,
+        string $language,
+        int $limit,
+        int $stepSize
+    ): array {
         $categoriesFetchedInMultipleRequests = [];
         for ($offset = 0; $offset < $limit; $offset += $stepSize) {
             $categoriesFetchedInMultipleRequests = array_merge(
                 $categoriesFetchedInMultipleRequests,
-                $productApi->getCategories(new CategoryQuery([
-                    'locale' => $language,
-                    'limit' => $stepSize,
-                    'offset' => $offset,
-                ]))
+                $this->fetchCategories($project, $language, $stepSize, $offset)
             );
         }
+        return $categoriesFetchedInMultipleRequests;
+    }
 
-        usort(
-            $categoriesFetchedInOneRequest,
-            function (Category $lhs, Category $rhs) {
-                return strcmp($lhs->categoryId, $rhs->categoryId);
-            }
-        );
-        usort(
-            $categoriesFetchedInMultipleRequests,
-            function (Category $lhs, Category $rhs) {
-                return strcmp($lhs->categoryId, $rhs->categoryId);
-            }
-        );
+    /**
+     * @param Category[] $expected
+     * @param Category[] $actual
+     */
+    private function assertCategoriesEqualIgnoringOrder(array $expected, array $actual): void
+    {
+        $compareCategoryIds = function (Category $lhs, Category $rhs) {
+            return strcmp($lhs->categoryId, $rhs->categoryId);
+        };
+        usort($expected, $compareCategoryIds);
+        usort($actual, $compareCategoryIds);
 
-        $this->assertGreaterThan($stepSize, count($categoriesFetchedInMultipleRequests));
-        $this->assertEquals($categoriesFetchedInOneRequest, $categoriesFetchedInMultipleRequests);
+        $this->assertEquals($expected, $actual);
     }
 }
