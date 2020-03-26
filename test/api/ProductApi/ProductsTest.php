29019569d884412f26bb3b387e2d6e20e08d5916
Florian Sowade <f.sowade@suora.com>
2020-03-26 21:36:22 +0100

    Enhance error messages

--- test/api/ProductApi/ProductsTest.php
+++ test/api/ProductApi/ProductsTest.php
@@ -467,9 +467,21 @@ class ProductsTest extends FrontasticApiTestCase
         // the information from the search index might be outdated.
 
         $this->assertSame($expected->productId, $actual->productId);
-        $this->assertSame($expected->name, $actual->name);
-        $this->assertSame($expected->slug, $actual->slug);
-        $this->assertSame($expected->description, $actual->description);
+        $this->assertSame(
+            $expected->name,
+            $actual->name,
+            sprintf('Product %s (SKU %s) has different names', $expected->productId, $expected->sku)
+        );
+        $this->assertSame(
+            $expected->slug,
+            $actual->slug,
+            sprintf('Product %s (SKU %s) has different slugs', $expected->productId, $expected->sku)
+        );
+        $this->assertSame(
+            $expected->description,
+            $actual->description,
+            sprintf('Product %s (SKU %s) has different descriptions', $expected->productId, $expected->sku)
+        );
 
         $this->assertSameSize($expected->variants, $actual->variants);
         for ($variantIndex = 0; $variantIndex < count($expected->variants); ++$variantIndex) {
