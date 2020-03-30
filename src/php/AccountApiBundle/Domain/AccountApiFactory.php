bfefd2856e359183b51f96b2b7e5b92029fa32fe
Florian Sowade <f.sowade@suora.com>
2020-03-30 12:06:10 +0200

    Use project  in AccountApiFactory

--- src/php/AccountApiBundle/Domain/AccountApiFactory.php
+++ src/php/AccountApiBundle/Domain/AccountApiFactory.php
@@ -3,42 +3,39 @@
 namespace Frontastic\Common\AccountApiBundle\Domain;
 
 use Doctrine\Common\Cache\Cache;
-use Frontastic\Common\HttpClient;
-use Frontastic\Common\ProductApiBundle\Domain\ProductApi\Commercetools\Client;
-use Frontastic\Common\ReplicatorBundle\Domain\Customer;
+use Frontastic\Common\ProductApiBundle\Domain\ProductApi\Commercetools\ClientFactory;
+use Frontastic\Common\ReplicatorBundle\Domain\Project;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class AccountApiFactory
 {
+    /** @var ContainerInterface */
     private $container;
-    private $cache;
+
     private $decorators = [];
 
-    public function __construct($container, Cache $cache, iterable $decorators)
+    public function __construct(ContainerInterface $container, iterable $decorators)
     {
         $this->container = $container;
-        $this->cache = $cache;
         $this->decorators = $decorators;
     }
 
-    public function factor(Customer $customer): AccountApi
+    public function factor(Project $project): AccountApi
     {
-        switch ($customer->configuration['account']->engine) {
+        $accountConfig = $project->getConfigurationSection('account');
+
+        switch ($accountConfig->engine) {
             case 'commercetools':
-                $accountApi = new AccountApi\Commercetools(
-                    new Client(
-                        $customer->configuration['account']->clientId,
-                        $customer->configuration['account']->clientSecret,
-                        $customer->configuration['account']->projectKey,
-                        $customer->configuration['account']->hostUrl ?? 'https://api.sphere.io',
-                        $this->container->get(HttpClient::class),
-                        $this->cache
-                    )
-                );
+                $client = $this->container
+                    ->get(ClientFactory::class)
+                    ->factorForProjectAndType($project, 'account');
+
+                $accountApi = new AccountApi\Commercetools($client);
                 break;
 
             default:
                 throw new \OutOfBoundsException(
-                    "No account API configured for customer {$customer->name}. " .
+                    "No account API configured for project {$project->name}. " .
                     "Check the provisioned customer configuration in app/config/customers/."
                 );
         }
