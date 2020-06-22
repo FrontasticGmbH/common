a36695fc78fea5ab5586854a22e7224a3b8ba4ed
Carlos Mourullo <carlos@frontastic.cloud>
2020-06-22 15:46:07 +0100

    Created SprykerBundle skeleton

---
+++ src/php/SprykerBundle/Domain/ClientInterface.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Frontastic\Common\SprykerBundle\Domain;
+
+interface ClientInterface
+{
+    public const MODE_SYNC = 'sync';
+    public const MODE_ASYNC = 'async';
+
+    public const METHOD_GET = 'GET';
+    public const METHOD_HEAD = 'HEAD';
+    public const METHOD_POST = 'POST';
+    public const METHOD_DELETE = 'DELETE';
+    public const METHOD_PUT = 'PUT';
+    public const METHOD_PATCH = 'PATCH';
+
+    /**
+     * @param string $endpoint
+     * @param array $headers
+     * @param string $mode
+     * @return mixed
+     */
+    public function get(string $endpoint, array $headers = [], string $mode = self::MODE_SYNC);
+
+    /**
+     * @param string $endpoint
+     * @param array $headers
+     * @return mixed
+     */
+    public function head(string $endpoint, array $headers = []);
+
+    /**
+     * @param string $endpoint
+     * @param array $headers
+     * @param string $body
+     * @param string $mode
+     * @return mixed
+     */
+    public function post(
+        string $endpoint,
+        array $headers = [],
+        string $body = '',
+        string $mode = self::MODE_SYNC
+    );
+
+    /**
+     * @param string $endpoint
+     * @param array $headers
+     * @param string $body
+     * @return mixed
+     */
+    public function patch(string $endpoint, array $headers = [], string $body = '');
+
+    /**
+     * @param string $endpoint
+     * @param array $headers
+     * @return mixed
+     */
+    public function delete(string $endpoint, array $headers = []);
+}
