b38336044ff41ad9bf34fbf9a08a7e6165e1255b
Florian Sowade <f.sowade@suora.com>
2020-08-31 16:53:10 +0200

    feat(searchApi): add livecycle event decorator

--- src/php/ProductSearchApiBundle/Domain/DefaultProductSearchApiFactory.php
+++ src/php/ProductSearchApiBundle/Domain/DefaultProductSearchApiFactory.php
@@ -5,24 +5,24 @@ namespace Frontastic\Common\ProductSearchApiBundle\Domain;
 use Frontastic\Common\FindologicBundle\Domain\FindologicClientFactory;
 use Frontastic\Common\FindologicBundle\Domain\ProductSearchApi\FindologicProductSearchApi;
 use Frontastic\Common\ReplicatorBundle\Domain\Project;
-
 use Psr\Container\ContainerInterface;
 
-/**
- * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
- */
 class DefaultProductSearchApiFactory implements ProductSearchApiFactory
 {
     private const CONFIGURATION_TYPE_NAME = 'product';
 
-    /**
-     * @var \Psr\Container\ContainerInterface
-     */
+    /** @var ContainerInterface */
     private $container;
 
-    public function __construct(ContainerInterface $container)
-    {
+    /** @var array */
+    private $decorators;
+
+    public function __construct(
+        ContainerInterface $container,
+        iterable $decorators = []
+    ) {
         $this->container = $container;
+        $this->decorators = $decorators;
     }
 
     public function factor(Project $project): ProductSearchApi
@@ -38,11 +38,11 @@ class DefaultProductSearchApiFactory implements ProductSearchApiFactory
                 break;
             default:
                 throw new \OutOfBoundsException(
-                    "No product API configured for project {$project->name}. " .
+                    "No product search API configured for project {$project->name}. " .
                     "Check the provisioned customer configuration in app/config/customers/."
                 );
         }
 
-        return $productSearchApi;
+        return new LifecycleEventDecorator($productSearchApi, $this->decorators);
     }
 }
