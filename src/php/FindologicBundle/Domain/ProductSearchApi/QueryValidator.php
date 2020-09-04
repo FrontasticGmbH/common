1f0534d671977cd7b52e56db30aa29fd634465d8
Jan Krausenbaum <j.krausenbaum@suora.com>
2020-09-04 09:42:47 +0200

    feat(productSearchApi): Don’t support SKU search at all for Findologic and simplify validator

--- src/php/FindologicBundle/Domain/ProductSearchApi/QueryValidator.php
+++ src/php/FindologicBundle/Domain/ProductSearchApi/QueryValidator.php
@@ -16,26 +16,20 @@ class QueryValidator
             return ValidationResult::createUnsupported('Sorting by more than one field is not supported.');
         }
 
-        if (!empty($query->skus) && count($query->skus) > 1) {
-            return ValidationResult::createUnsupported('Searching for multiple SKUs is not supported.');
+        if (!empty($query->sku) || !empty($query->skus)) {
+            return ValidationResult::createUnsupported('Searching for SKUs is not supported.');
         }
 
         if (!empty($query->productIds) && count($query->productIds) > 1) {
             return ValidationResult::createUnsupported('Searching for multiple productIds is not supported.');
         }
 
-        if (!empty($query->productIds) && !empty($query->skus)) {
-            return ValidationResult::createUnsupported(
-                'Searching for productIds and SKUs at the same time is not supported'
-            );
-        }
-
         $hasCriteria = !empty($query->query) || !empty($query->facets) || !empty($query->filter);
-        $hasIds = !empty($query->skus) || !empty($query->sku) || !empty($query->productIds) || !empty($query->productId);
+        $hasIds = !empty($query->productIds) || !empty($query->productId);
 
         if ($hasCriteria && $hasIds) {
             return ValidationResult::createUnsupported(
-                'Searching for productIds or SKUs with additional query parameters is not supported'
+                'Searching for productIds with additional query parameters is not supported'
             );
         }
 
