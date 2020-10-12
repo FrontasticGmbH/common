5d79d1edf54d8e7ceceb80e2afee698075cdb36f
Kore Nordmann <mail@kore-nordmann.de>
2020-10-12 12:50:07 +0200

    fix: Do not use return value of test skip

--- test/php/JsonSerializerTest.php
+++ test/php/JsonSerializerTest.php
@@ -66,7 +66,7 @@ class JsonSerializerTest extends \PHPUnit\Framework\TestCase
     public function testStripTraceParameters()
     {
         if (version_compare(PHP_VERSION, '7.4.0') >= 0) {
-            return $this->markTestSkipped('Some ReflectionClass error with PHP 7.4');
+            $this->markTestSkipped('Some ReflectionClass error with PHP 7.4');
         }
 
         $serializer = new JsonSerializer();
