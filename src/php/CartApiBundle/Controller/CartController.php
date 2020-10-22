eb8d278e5ae9aebf204eccc296f54b69bfab6e7c
Florian Sowade <f.sowade@suora.com>
2020-10-22 14:21:35 +0200

    fix: Don't fail on missing address data

--- src/php/CartApiBundle/Controller/CartController.php
+++ src/php/CartApiBundle/Controller/CartController.php
@@ -223,13 +223,13 @@ class CartController extends CrudController
         if (!empty($payload['shipping']) || !empty($payload['billing'])) {
             $cart = $cartApi->setShippingAddress(
                 $cart,
-                Address::newWithProjectSpecificData($payload['shipping'] ?: $payload['billing']),
+                Address::newWithProjectSpecificData(($payload['shipping'] ?? []) ?: $payload['billing']),
                 $context->locale
             );
 
             $cart = $cartApi->setBillingAddress(
                 $cart,
-                Address::newWithProjectSpecificData($payload['billing'] ?: $payload['shipping']),
+                Address::newWithProjectSpecificData(($payload['billing'] ?? []) ?: $payload['shipping']),
                 $context->locale
             );
         }
