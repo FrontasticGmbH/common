78707033933488e124d878a75a0e2f72dbf52ce2
Carlos Mourullo <carlos@frontastic.cloud>
2020-11-02 12:58:10 +0000

    fix(spryker): Replaced attribute value by key and label

--- src/php/SprykerBundle/Common/NestedAttributeValueTransformExpanderTrait.php
+++ src/php/SprykerBundle/Common/NestedAttributeValueTransformExpanderTrait.php
@@ -29,9 +29,10 @@ trait NestedAttributeValueTransformExpanderTrait
     {
         foreach ($variant->attributes as $key => $value) {
             if (strpos($key, '_label_') === 0) {
-                $realKey = str_replace('_label_', '', $key);
-                $this->attributeValueToNested($variant, $realKey);
-                $variant->attributes[$realKey]['label'] = $value;
+                // TODO: Handle labels with localisation
+                // $realKey = str_replace('_label_', '', $key);
+                // $this->attributeValueToNested($variant, $realKey);
+                // $variant->attributes[$realKey]['label'] = $value;
                 unset($variant->attributes[$key]);
             } else {
                 $this->attributeValueToNested($variant, $key);
@@ -56,7 +57,10 @@ trait NestedAttributeValueTransformExpanderTrait
         $value = $variant->attributes[$key] ?? null;
 
         if (!is_array($value)) {
-            $variant->attributes[$key] = ['value' => $value];
+            $variant->attributes[$key] = [
+                'key' => $value,
+                'label' => $value
+            ];
         }
     }
 }
