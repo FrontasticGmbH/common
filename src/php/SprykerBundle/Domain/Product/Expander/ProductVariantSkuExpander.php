0ffa4b6ee7f87537e5e907dac101fde022eaeb3c
Carlos Mourullo <carlos@frontastic.cloud>
2020-11-02 09:59:06 +0000

    refactor(spryker): Removed unused methods and extra comments

--- src/php/SprykerBundle/Domain/Product/Expander/ProductVariantSkuExpander.php
+++ src/php/SprykerBundle/Domain/Product/Expander/ProductVariantSkuExpander.php
@@ -3,6 +3,7 @@
 namespace Frontastic\Common\SprykerBundle\Domain\Product\Expander;
 
 use Frontastic\Common\ProductApiBundle\Domain\Product;
+use Frontastic\Common\ProductApiBundle\Domain\Variant;
 use Frontastic\Common\SprykerBundle\Domain\Product\SprykerProductApiExtendedConstants;
 use WoohooLabs\Yang\JsonApi\Schema\Resource\ResourceObject;
 
@@ -14,56 +15,28 @@ class ProductVariantSkuExpander implements ProductExpanderInterface
 {
     use IncludedResourceExpanderTrait;
 
-    /**
-     * @param \Frontastic\Common\ProductApiBundle\Domain\Product|null $product
-     * @param array|\WoohooLabs\Yang\JsonApi\Schema\Resource\ResourceObject[] $includes
-     *
-     * @return \Frontastic\Common\ProductApiBundle\Domain\Product|null
-     */
     public function expand(Product $product, array $includes): Product
     {
         $concreteProduct = $this->getResourceIncludeByAttributeKey(
             $includes,
             SprykerProductApiExtendedConstants::PRODUCT_RESOURCE_CONCRETE_PRODUCTS,
             'productAbstractSku',
-            $this->getProductId($product)
+            $product->productId
         );
 
         if (!$concreteProduct) {
             return $product;
         }
 
-        $this->expandVariants($product->variants, $concreteProduct);
+        $this->expandVariant($product->variants[0], $concreteProduct);
 
         return $product;
     }
 
-    protected function getResourceIncludeByProductId(array $includes, string $type, string $resourceId): ?ResourceObject
+    protected function expandVariant(Variant $variant, ResourceObject $resource): void
     {
-        foreach ($includes as $include) {
-            if ($include->type() === $type && $include->attributes()['productAbstractSku'] === $resourceId) {
-                return $include;
-            }
-        }
-
-        return null;
-    }
-
-    protected function getProductId(Product $product): string
-    {
-        return $product->productId;
-    }
-
-    /**
-     * @param array|\Frontastic\Common\ProductApiBundle\Domain\Variant[] $variants
-     * @param \WoohooLabs\Yang\JsonApi\Schema\Resource\ResourceObject $resource
-     *
-     * @return void
-     */
-    protected function expandVariants(array $variants, ResourceObject $resource): void
-    {
-        $variants[0]->id = $resource->id();
-        $variants[0]->sku = $resource->attributes()['sku'];
-        $variants[0]->groupId = $resource->attributes()['sku'];
+        $variant->id = $resource->id();
+        $variant->sku = $resource->attributes()['sku'];
+        $variant->groupId = $resource->attributes()['sku'];
     }
 }
