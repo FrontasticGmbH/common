acc0de96c679370bdda750ec9f71bda8e466d1a1
Henrique Barroso <henrique@frontastic.cloud>
2020-12-03 10:59:15 +0100

    [FT-357] Install SIMDjson

---
+++ src/php/CoreBundle/Domain/Json/Json.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace Frontastic\Common\CoreBundle\Domain\Json;
+
+
+class Json implements JsonInterface
+{
+    /**
+     * @param $data
+     * @param int $flags
+     * @param int $depth
+     * @return false|string
+     * @throws InvalidJsonEncodeException
+     */
+    public static function encode($data, int $flags = 0, int $depth = 512)
+    {
+        $result = json_encode($data, $flags, $depth);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new InvalidJsonEncodeException(json_last_error());
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param $data
+     * @param false $associative
+     * @param int $depth
+     * @param int $flags
+     * @param bool $useNativeDecoder
+     * @return mixed
+     * @throws InvalidJsonDecodeException
+     */
+    public static function decode($data, $associative = false, int $depth = 512, int $flags = 0, $useNativeDecoder = false)
+    {
+        if (!$useNativeDecoder && function_exists('simdjson_decode')) {
+            try {
+                return simdjson_decode($data, $associative, $depth);
+            } catch (\Exception $e) {
+                throw new InvalidJsonDecodeException($e->getMessage(), $e->getCode());
+            }
+        }
+
+        $result = json_decode($data, $associative, $depth, $flags);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new InvalidJsonDecodeException(json_last_error());
+        }
+
+        return $result;
+    }
+}
---
+++ src/php/CoreBundle/Domain/Json/JsonInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Frontastic\Common\CoreBundle\Domain\Json;
+
+
+interface JsonInterface
+{
+    /**
+     * @param $data
+     * @param int $flags
+     * @param int $depth
+     * @return string|false
+     * @throws InvalidJsonEncodeException
+     */
+    public static function encode($data, int $flags, int $depth);
+
+    /**
+     * @param $data
+     * @param $associative
+     * @param int $depth
+     * @param int $flags
+     * @param $useNativeDecoder
+     * @return mixed
+     * @throws InvalidJsonDecodeException
+     */
+    public static function decode($data, $associative, int $depth, int $flags, $useNativeDecoder);
+}
