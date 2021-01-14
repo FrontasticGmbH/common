d5b4976ced9e394385a8b7f38e5e32fbc6f672d7
Tobias Schlitt <toby@frontastic.cloud>
2021-01-14 09:32:17 +0100

    fix: CS.

--- src/php/CartApiBundle/Domain/CartApi/Commercetools/Options.php
+++ src/php/CartApiBundle/Domain/CartApi/Commercetools/Options.php
@@ -13,7 +13,8 @@ class Options extends DataObject
     /**
      * HashMap of CommerceTools cart default values.
      *
-     * Will be merged with Frontastic set data when a new cart is created. Frontastic data override values in this array.
+     * Will be merged with Frontastic set data when a new cart is created.
+     * Frontastic data override values in this array.
      * See https://docs.commercetools.com/api/projects/carts for details
      *
      * @var mixed[string]
