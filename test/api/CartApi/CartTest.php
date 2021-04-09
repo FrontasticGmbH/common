5c6f2b20b487baeb13d564e7edc2d7d595ef3e7c
Carlos Mourullo <carlos@frontastic.cloud>
2021-04-09 17:13:45 +0200

    misc: included API test to place order for logged in user

--- test/api/CartApi/CartTest.php
+++ test/api/CartApi/CartTest.php
@@ -3,12 +3,11 @@
 namespace Frontastic\Common\ApiTests\CartApi;
 
 use Frontastic\Common\AccountApiBundle\Domain\Account;
+use Frontastic\Common\AccountApiBundle\Domain\Address;
 use Frontastic\Common\ApiTests\FrontasticApiTestCase;
 use Frontastic\Common\CartApiBundle\Domain\Cart;
-use Frontastic\Common\CartApiBundle\Domain\LineItem;
+use Frontastic\Common\CartApiBundle\Domain\Order;
 use Frontastic\Common\CartApiBundle\Domain\ShippingMethod;
-use Frontastic\Common\ProductApiBundle\Domain\Product;
-use Frontastic\Common\ProductApiBundle\Domain\Variant;
 use Frontastic\Common\ReplicatorBundle\Domain\Project;
 
 class CartTest extends FrontasticApiTestCase
@@ -24,7 +23,6 @@ class CartTest extends FrontasticApiTestCase
         $this->assertNotEmptyString($cart->cartId);
         $this->assertNotEmptyString($cart->cartVersion);
 
-
         if ($cart->projectSpecificData !== null) {
             $this->assertSame([], $cart->projectSpecificData);
         }
@@ -36,15 +34,79 @@ class CartTest extends FrontasticApiTestCase
         $this->assertSame([], $cart->discountCodes);
     }
 
+    /**
+     * @dataProvider projectAndLanguage
+     */
+    public function testOrderSingleProduct(Project $project, string $language): void
+    {
+        $account = $this->getAccount($project, $language);
+        $cart = $this->getCart($project, $account, $language);
+
+        $cartApi = $this->getCartApiForProject($project);
+
+        $cartApi->startTransaction($cart);
+        $cartApi->addToCart($cart, $this->getLineItemForProduct($this->getAProduct($project, $language)), $language);
+        try {
+            $cartApi->setShippingAddress($cart, $this->getFrontasticAddress(), $language);
+        } catch (\RuntimeException $e) {
+            // In case that the backend didn't implement CartApi::setShippingAddress,
+            // catch the exception but do nothing
+        }
+        $cart = $cartApi->commit($language);
+
+        $order = $cartApi->order($cart, $language);
+
+        $this->assertInstanceOf(Order::class, $order);
+        $this->assertNotNull($order->orderId);
+        $this->assertNotNull($order->orderVersion);
+        $this->assertNotNull($order->orderState);
+        $this->assertInstanceOf(\DateTimeImmutable::class, $order->createdAt);
+        $this->assertGreaterThan(
+            (new \DateTimeImmutable())->sub(new \DateInterval('PT2H')),
+            $order->createdAt
+        );
+        $this->assertLessThan(
+            (new \DateTimeImmutable())->add(new \DateInterval('PT2H')),
+            $order->createdAt
+        );
+    }
+
+    private function getTestAddressData(?string $salutation = null): array
+    {
+        return [
+            'salutation' => $salutation,
+            'firstName' => 'Ashley',
+            'lastName' => 'Stoltenberg',
+            'streetName' => 'Test str.',
+            'streetNumber' => '11',
+            'additionalAddressInfo' => 'Additional addr info',
+            'additionalStreetInfo' => 'Additional str info',
+            'postalCode' => '123456',
+            'city' => 'Berlin',
+            'country' => 'DE',
+            'phone' => '+49 12 1234 12234',
+            'isDefaultBillingAddress' => true,
+            'isDefaultShippingAddress' => true,
+        ];
+    }
+
     private function getAccount(Project $project, string $language): Account
     {
+        $salutation = 'Frau';
+        if (($salutations = $this->getAccountApiForProject($project)->getSalutations($language)) !== null) {
+            $salutation = $salutations[array_rand($salutations)];
+        }
+
         $account = new Account([
             'email' => 'integration-tests-not-exists+account-' . uniqid('', true) . '@frontastic.com',
-            'salutation' => 'Frau',
+            'salutation' => $salutation,
             'firstName' => 'Ashley',
             'lastName' => 'Stoltenberg',
             'birthday' => new \DateTimeImmutable('1961-11-6'),
             'confirmed' => false,
+            'addresses' => [
+                new Address($this->getTestAddressData($salutation)),
+            ],
         ]);
         $account->setPassword('cHAaL4Pd.4yCcwLR');
 
