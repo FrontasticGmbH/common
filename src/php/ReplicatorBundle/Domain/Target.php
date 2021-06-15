44d805a15beb0582fc126403940b60f9e578174d
Tobias Schlitt <toby@frontastic.cloud>
2021-06-15 12:57:35 +0200

    chore: Docs for replicator interfaces.

--- src/php/ReplicatorBundle/Domain/Target.php
+++ src/php/ReplicatorBundle/Domain/Target.php
@@ -4,7 +4,14 @@ namespace Frontastic\Common\ReplicatorBundle\Domain;
 
 interface Target
 {
+    /**
+     * Returns the latest sequence revision securely stored in the Target.
+     */
     public function lastUpdate(): string;
 
+    /**
+     * Store all changes in $updates in the corresponding order. Throw
+     * exception if a change cannot be stored.
+     */
     public function replicate(array $updates): void;
 }
