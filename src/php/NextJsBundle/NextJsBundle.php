95aeb97931d94d4196ed98d60c0867fad1b0f7c2
Volker Dusch <volker@frontastic.cloud>
2021-07-15 13:48:02 +0200

    feat: Bundle for NextJs

---
+++ src/php/NextJsBundle/NextJsBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Frontastic\Common\NextJsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NextJsBundle extends Bundle
+{
+    /**
+     * @return ?string
+     */
+    public function getParent()
+    {
+        return null;
+    }
+}
