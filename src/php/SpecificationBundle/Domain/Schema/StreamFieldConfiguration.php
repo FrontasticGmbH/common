5103dcf3d311f048580746c0e080030335277e48
jim <jim@frontastic.cloud>
2021-12-15 13:08:16 +0200

    chore: CS, optional stream type

--- src/php/SpecificationBundle/Domain/Schema/StreamFieldConfiguration.php
+++ src/php/SpecificationBundle/Domain/Schema/StreamFieldConfiguration.php
@@ -6,13 +6,13 @@ use Frontastic\Common\SpecificationBundle\Domain\ConfigurationSchema;
 
 class StreamFieldConfiguration extends FieldConfiguration
 {
-    private string $streamType;
+    private ?string $streamType;
 
     protected static function doCreateFromSchema(string $type, array $fieldSchema): FieldConfiguration
     {
         /** @var StreamFieldConfiguration $schema */
         $schema = parent::doCreateFromSchema($type, $fieldSchema);
-        $schema->streamType = $fieldSchema['streamType'];
+        $schema->streamType = $fieldSchema['streamType'] ?? null;
         return $schema;
     }
 
