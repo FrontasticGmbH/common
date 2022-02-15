876eecfd3862829034aaabb871d5c682fdf7d4a3
Sven Koschnicke <s.koschnicke@gfxpro.com>
2022-02-15 15:10:51 +0100

    fix(api-hub): replace repeating exception with function in DummyAccountApi

--- src/php/AccountApiBundle/Domain/AccountApi/DummyAccountApi.php
+++ src/php/AccountApiBundle/Domain/AccountApi/DummyAccountApi.php
@@ -12,22 +12,22 @@ class DummyAccountApi implements AccountApi
 {
     public function getSalutations(string $locale): ?array
     {
-        throw new \Exception("AccountApi is not available for Nextjs projects.");
+        $this->doNotUse();
     }
 
     public function confirmEmail(string $token, string $locale = null): Account
     {
-        throw new \Exception("AccountApi is not available for Nextjs projects.");
+        $this->doNotUse();
     }
 
     public function create(Account $account, ?Cart $cart = null, string $locale = null): Account
     {
-        throw new \Exception("AccountApi is not available for Nextjs projects.");
+        $this->doNotUse();
     }
 
     public function update(Account $account, string $locale = null): Account
     {
-        throw new \Exception("AccountApi is not available for Nextjs projects.");
+        $this->doNotUse();
     }
 
     public function updatePassword(
@@ -36,60 +36,65 @@ class DummyAccountApi implements AccountApi
         string $newPassword,
         string $locale = null
     ): Account {
-        throw new \Exception("AccountApi is not available for Nextjs projects.");
+        $this->doNotUse();
     }
 
     public function generatePasswordResetToken(string $email): PasswordResetToken
     {
-        throw new \Exception("AccountApi is not available for Nextjs projects.");
+        $this->doNotUse();
     }
 
     public function resetPassword(string $token, string $newPassword, string $locale = null): Account
     {
-        throw new \Exception("AccountApi is not available for Nextjs projects.");
+        $this->doNotUse();
     }
 
     public function login(Account $account, ?Cart $cart = null, string $locale = null): ?Account
     {
-        throw new \Exception("AccountApi is not available for Nextjs projects.");
+        $this->doNotUse();
     }
 
     public function refreshAccount(Account $account, string $locale = null): Account
     {
-        throw new \Exception("AccountApi is not available for Nextjs projects.");
+        $this->doNotUse();
     }
 
     public function getAddresses(Account $account, string $locale = null): array
     {
-        throw new \Exception("AccountApi is not available for Nextjs projects.");
+        $this->doNotUse();
     }
 
     public function addAddress(Account $account, Address $address, string $locale = null): Account
     {
-        throw new \Exception("AccountApi is not available for Nextjs projects.");
+        $this->doNotUse();
     }
 
     public function updateAddress(Account $account, Address $address, string $locale = null): Account
     {
-        throw new \Exception("AccountApi is not available for Nextjs projects.");
+        $this->doNotUse();
     }
 
     public function removeAddress(Account $account, string $addressId, string $locale = null): Account
     {
-        throw new \Exception("AccountApi is not available for Nextjs projects.");
+        $this->doNotUse();
     }
 
     public function setDefaultBillingAddress(Account $account, string $addressId, string $locale = null): Account
     {
-        throw new \Exception("AccountApi is not available for Nextjs projects.");
+        $this->doNotUse();
     }
 
     public function setDefaultShippingAddress(Account $account, string $addressId, string $locale = null): Account
     {
-        throw new \Exception("AccountApi is not available for Nextjs projects.");
+        $this->doNotUse();
     }
 
     public function getDangerousInnerClient()
+    {
+        $this->doNotUse();
+    }
+
+    private function doNotUse(): void
     {
         throw new \Exception("AccountApi is not available for Nextjs projects.");
     }
