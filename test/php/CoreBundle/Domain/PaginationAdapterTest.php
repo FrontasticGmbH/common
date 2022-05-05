569059fa95656ab78521d3b1f0558f426de0d632
Tobias Schlitt <toby@frontastic.cloud>
2022-05-05 13:32:55 +0200

    fix(FP-1934): Test basic PaginationAdapterBehavior.

---
+++ test/php/CoreBundle/Domain/PaginationAdapterTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Frontastic\Common\CoreBundle\Domain;
+
+use Frontastic\Common\ProductApiBundle\Domain\ProductApi\Result;
+use PHPUnit\Framework\TestCase;
+
+class PaginationAdapterTest extends TestCase
+{
+    public function testResultOffsetToCursorFirstPage()
+    {
+        $resultFixture = new Result();
+        $resultFixture->offset = 0;
+        $resultFixture->count = 100;
+        $resultFixture->total = 1000;
+
+        PaginationAdapter::resultOffsetToCursor($resultFixture);
+
+        $this->assertSame('offset:100', $resultFixture->nextCursor);
+        $this->assertNull($resultFixture->previousCursor);
+    }
+
+    public function testResultOffsetToCursorPageInBetween()
+    {
+        $resultFixture = new Result();
+        $resultFixture->offset = 300;
+        $resultFixture->count = 100;
+        $resultFixture->total = 1000;
+
+        PaginationAdapter::resultOffsetToCursor($resultFixture);
+
+        $this->assertSame('offset:400', $resultFixture->nextCursor);
+        $this->assertSame('offset:200', $resultFixture->previousCursor);
+    }
+
+    public function testResultOffsetToCursorPageLastPage()
+    {
+        $resultFixture = new Result();
+        $resultFixture->offset = 900;
+        $resultFixture->count = 100;
+        $resultFixture->total = 1000;
+
+        PaginationAdapter::resultOffsetToCursor($resultFixture);
+
+        $this->assertNull( $resultFixture->nextCursor);
+        $this->assertSame('offset:800', $resultFixture->previousCursor);
+    }
+}
