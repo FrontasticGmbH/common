120628635e78512f33207cf5afd627f83cf6c885
Joel Fickson <joelngozo@gmail.com>
2023-02-01 11:44:26 +0200

    feat: update formatting

--- src/php/CoreBundle/Domain/Mailer.php
+++ src/php/CoreBundle/Domain/Mailer.php
@@ -6,5 +6,11 @@ use Symfony\Component\Templating\EngineInterface;
 
 abstract class Mailer
 {
-    abstract public function sendToUser($user, string $type, string $subject, array $parameters = array(), $ignoreErrors = false);
+    abstract public function sendToUser(
+        $user,
+        string $type,
+        string $subject,
+        array $parameters = array(),
+        $ignoreErrors = false
+    );
 }
