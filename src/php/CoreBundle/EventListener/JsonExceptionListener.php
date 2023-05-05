599dacf073d88c90387817ce81cf705ee1438003
Aly Muhammad Aly <aly.radwan@frontastic.cloud>
2023-05-05 14:14:41 +0300

    Create listner for invalid context exceptions

--- src/php/CoreBundle/EventListener/JsonExceptionListener.php
+++ src/php/CoreBundle/EventListener/JsonExceptionListener.php
@@ -27,6 +27,7 @@ class JsonExceptionListener
         if (!in_array('application/json', $acceptableContentTypes) &&
             !in_array('text/json', $acceptableContentTypes) &&
             !$event->getRequest()->isXmlHttpRequest()) {
+            // var_dump($acceptableContentTypes);
             return;
         }
 
