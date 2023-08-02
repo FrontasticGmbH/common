771d7eaeae5e6fd1ceb5dd86043155c78a75678e
Christian Hammerl <c.hammerl@suora.com>
2023-08-02 15:21:47 +0200

    feat: use shopify api version 2023-01

--- src/php/ShopifyBundle/Domain/ShopifyClient.php
+++ src/php/ShopifyBundle/Domain/ShopifyClient.php
@@ -12,7 +12,7 @@ use Frontastic\Common\CoreBundle\Domain\Json\Json;
 
 class ShopifyClient
 {
-    const SHOPIFY_API_VERSION = "2022-10";
+    const SHOPIFY_API_VERSION = "2023-01";
 
     /**
      * @var string
